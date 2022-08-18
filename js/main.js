16b4ac9f997600bf12f73f9c5071381a136b5aa5
askomarov <askomarov13@mail.ru>
2022-08-18 19:26:54 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 console.log('hello');
+
+const promoText = document.querySelector('.promo__text');
+
+
 let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
 
@@ -13,3 +17,60 @@ navToggle.addEventListener("click", function () {
     this.setAttribute("aria-expanded", "true");
   }
 });
+
+const URL = 'https://baconipsum.com/api/?type=lucky';
+
+function getTextData(cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', URL);
+  xhr.addEventListener('load', () => {
+    const response = JSON.parse(xhr.responseText);
+    cb(response);
+  });
+
+  xhr.addEventListener('error', () => {
+    console.log('error');
+  });
+
+  xhr.send();
+};
+
+function renderPromoText(response) {
+  const dataText = response;
+  promoText.textContent = dataText[0];
+  return;
+};
+
+getTextData(renderPromoText);
+
+const searchForm = document.forms['search-form'];
+const inputSeacrh = searchForm.elements['id-search'];
+
+let inputValid = false;
+
+inputSeacrh.addEventListener('input', () => {
+  if (useRegex(inputSeacrh.value)) {
+    console.log('data very bad');
+    inputSeacrh.style.boxShadow = 'inset 0 0 3px 1px red';
+    inputSeacrh.setCustomValidity('Введены запрщенные символы! Попробуйте ввести только буквы и цифры');
+    return;
+  } else {
+    console.log('good data');
+    inputSeacrh.setCustomValidity('');
+    inputSeacrh.style.boxShadow = '';
+  }
+  inputSeacrh.reportValidity();
+  return inputValid = true;
+})
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if (inputValid) {
+    searchForm.reset();
+  }
+})
+
+function useRegex(value) {
+  const regex = /^(?=.*[!@#$%^&()<>])/;
+  return regex.test(value);
+}
